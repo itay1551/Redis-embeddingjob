1224eb5cd6e038eb8afb9d4779f7f7ec409f9e0b
Itay Katav <ikatav@ikatav-thinkpadp16vgen1.raanaii.csb>
2024-12-30 09:20:13 +0200

    before chaning input

--- pipeline/main.py
+++ pipeline/main.py
@@ -9,14 +9,14 @@ IN_METHOD = 'IN_METHOD'
 AWS_SECRET = 'aws_secret'
 RAG_LLM_CM = 'rag_llm_cm'
 
-def save_docs(docs: List[Document], out_path: dsl.OutputPath):
+def save_docs(docs: List[Document], out_path: dsl.OutputPath(str)):
     import json
     with open(out_path, 'w') as f:
         json.dump(docs, f)
 
 # S3 bucket
-@dsl.component(base_image='TODO', packages_to_install=['boto3'])
-def load_data_from_s3(out_data_path: dsl.OutputPath):
+@dsl.component(base_image='registry.access.redhat.com/ubi9/python-311', packages_to_install=['boto3', 'langchain-community'])
+def load_data_from_s3(out_data_path: dsl.OutputPath(str)):
     import boto3
     import os
     from langchain_community.document_loaders import PyPDFLoader
@@ -32,9 +32,8 @@ def load_data_from_s3(out_data_path: dsl.OutputPath):
                       aws_access_key_id=aws_access_key_id,
                       aws_secret_access_key=aws_secret_access_key)
     
-    # assume tmp not exits
     tmp_dir = 'tmp_s3'
-    os.mkdir(tmp_dir)
+    os.makedirs(tmp_dir, exist_ok=True)
     
     docs = []
     continuation_token = None
@@ -49,7 +48,7 @@ def load_data_from_s3(out_data_path: dsl.OutputPath):
             
             for content in response.get('Contents', []):
                 key = content['key']
-                if not key.endwith('.pdf'):
+                if not key.endswith('.pdf'):
                     continue
                 
                 # download file from the bucket
@@ -66,7 +65,7 @@ def load_data_from_s3(out_data_path: dsl.OutputPath):
             
             # Check for more file
             if response['IsTruncated']:
-                continuation_token = response['ContinuationToken']
+                continuation_token = response['NextContinuationToken']
             else:
                 break
             
@@ -77,18 +76,19 @@ def load_data_from_s3(out_data_path: dsl.OutputPath):
 
     
 # Repository
-@dsl.component(base_image='TODO')
-def load_data_from_repo(out_data_path: dsl.OutputPath):
-    import subprocess
+@dsl.component(base_image='registry.access.redhat.com/ubi9/python-311', packages_to_install=['langchain-community', 'gitpython'])
+def load_data_from_repo(repo_url: str, docs_location: str, out_data_path: dsl.OutputPath(str)):
     from langchain_community.document_loaders import PyPDFDirectoryLoader
+    from git import Repo
     
-    repo_url = os.getenv('REPO_URL')
-    docs_location = os.getenv('REPO_DOC_LOCATION', '')
+    # repo_url = os.getenv('REPO_URL')
+    # docs_location = os.getenv('REPO_DOC_LOCATION', '')
     # Clone the repo to tmp_dir
     repo_path = os.path.join('tmp_repo', 'source_repo')
+    
     try:
-        subprocess.run(['git', 'clone', repo_url, repo_path])
-    except subprocess.CalledProcessError as e:
+        Repo.clone_from(repo_url, repo_path)
+    except Exception as e:
         print(f'Cloning error: {e}')
         
     docs_dir_path = os.path.join(repo_path, docs_location)
@@ -102,35 +102,35 @@ def load_data_from_repo(out_data_path: dsl.OutputPath):
     save_docs(docs, out_data_path)
 
 # URLs
-@dsl.component(base_image='TODO')
-def load_data_from_urls(urls: List[str], out_dir_path: dsl.OutputPath):
+@dsl.component(base_image='registry.access.redhat.com/ubi9/python-311', packages_to_install=['langchain-community'])
+def load_data_from_urls(urls: str, out_data_path: dsl.OutputPath(str)):
     from langchain_community.document_loaders import WebBaseLoader
     
     # Load URLs
     loader = WebBaseLoader(urls)
     docs = loader.load()
     
-    save_docs(docs, out_dir_path)
-        
-def load_docs(input_path: dsl.InputPath):
+    # save_docs(docs, out_data_path)
     import json
-    with open(input_path, 'r') as f:
-        return json.load(f)
+    with open(out_data_path, 'w') as f:
+        json.dump(docs, f)
 
-@dsl.component(base_image='TODO')
-def split_and_embed(input_docs_path: dsl.InputPath):
+@dsl.component(base_image='registry.access.redhat.com/ubi9/python-311', packages_to_install=['langchain-community'])
+def split_and_embed(input_docs_path: dsl.InputPath(str)):
     from langchain.text_splitter import RecursiveCharacterTextSplitter
     from vector_db.db_provider_factory import DBFactory
+    import json
     # Load docs
-    docs = load_docs(input_docs_path)
+    with open(input_docs_path, 'r') as f:
+        docs = json.load(f)
     
     # Split the documents
-    text_spliter = RecursiveCharacterTextSplitter(
+    text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1024,
         chunk_overlap=40,
         length_function=len,
     )
-    all_splits = text_spliter.split_documents(docs)
+    all_splits = text_splitter.split_documents(docs)
     
     # Indexing the VectorDB
     db_type = os.getenv(DB_TYPE)
@@ -138,38 +138,38 @@ def split_and_embed(input_docs_path: dsl.InputPath):
     vector_db.add_documents(all_splits)
 
 @dsl.pipeline(name=os.path.basename(__file__).replace('.py', ''))
-def rag_llm_pipeline():
-    input_method = os.getenv(IN_METHOD) #TODO Can I get it?
+def rag_llm_pipeline(input_method: str, repo_url: str, docs_location: str, urls: str):
+    # input_method = os.getenv(IN_METHOD) #TODO Can I get it?
     
     # Load the data
-    if input_method == 'repository':
-        load_data_task = load_data_from_repo()
-    elif input_method == 's3':
+    with dsl.If(input_method == 'repository'):
+        load_data_task = load_data_from_repo(repo_url=repo_url, docs_location=docs_location)
+    with dsl.Elif(input_method == 's3'):
         load_data_task = load_data_from_s3()     
         kubernetes.use_secret_as_env(
             task=load_data_task,
             secret_name=AWS_SECRET, # TODO change the secret name 
             secret_key_to_env={
-            'AWS_ACCESS_KEY_ID': 'AWS_ACCESS_KEY_ID',
-            'AWS_SECRET_ACCESS_KEY': 'AWS_SECRET_ACCESS_KEY',
-            'AWS_S3_BUCKET': 'AWS_S3_BUCKET',
-            'AWS_S3_ENDPOINT': 'AWS_S3_ENDPOINT'}
+                'AWS_ACCESS_KEY_ID': 'AWS_ACCESS_KEY_ID',
+                'AWS_SECRET_ACCESS_KEY': 'AWS_SECRET_ACCESS_KEY',
+                'AWS_S3_BUCKET': 'AWS_S3_BUCKET',
+                'AWS_S3_ENDPOINT': 'AWS_S3_ENDPOINT'}
             )
-    elif input_method == 'urls':
-        load_data_task = load_data_from_urls()
-    else:
-        raise ValueError('IN_METHOD env variable is missing or invalid')
+    # with dsl.Elif(input_method == 'urls'):
+    with dsl.Else():
+        load_data_task = load_data_from_urls(urls=urls)
+    # print(load_data_task.outputs['out_data'])
     load_data_task.set_caching_options(False)
-    kubernetes.use_config_map_as_env(load_data_task,
-                                 config_map_name=RAG_LLM_CM)
-                                # TODO I assume that if I dnot spesify below we take all the config args
-                                #  config_map_key_to_env={'foo': 'CM_VAR'})
+    # kubernetes.use_config_map_as_env(load_data_task,
+    #                              config_map_name=RAG_LLM_CM,
+    #                              config_map_key_to_env={'foo': 'CM_VAR'}) # TODO
     # Split embed
-    split_and_embed_task = split_and_embed(load_data_task.outputs['out_dir_path'])
+    split_and_embed_task = split_and_embed(input_docs_path=load_data_task.outputs['out_data_path'])
+    # split_and_embed_task = split_and_embed()
     split_and_embed_task.set_caching_options(False)
 
 if __name__ == '__main__':
-    compiler.Compiler.compile(
+    compiler.Compiler().compile(
         pipeline_func=rag_llm_pipeline,
         package_path=__file__.replace('.py', '.yaml')
     )
